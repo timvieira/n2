39f12ddd19c5705fd16b3d7f90ab6eccdd382cc7
timv <tim.f.vieira@gmail.com>
2016-11-22 11:29:21 -0500

    initial import

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='n2',
+      author='Tim Vieira',
+      description='Command-line tool for searching and indexing notes.',
+      version='1.0',
+      install_requires=[
+          'arsenal',
+          'path.py',
+          'whoosh',
+      ],
+      dependency_links=[
+          'https://github.com/timvieira/arsenal.git',
+      ],
+      scripts=['n2'],
+      #packages=['.'],
+)
